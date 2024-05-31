1c9831b8a05961d2ecb01902056915acf123fc24
Vipin8924 <vipinmhz8924@gmail.com>
2024-06-01 00:38:24 +0530

    Customer.java

--- rest-demo/src/main/java/com/project/rest_demo/model/Customer.java
+++ rest-demo/src/main/java/com/project/rest_demo/model/Customer.java
@@ -17,7 +17,8 @@ public class Customer {
     private String customerId;
     private String customerName;
     private String customerAddress;
-    private String customerPhoneNumber;
+    private Long customerPhoneNumber;
+    private int customerAge;
 
 
 
